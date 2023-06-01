e70df313a822831122cac6a7b08b38142bbb1957
Yoas-Hutapea <yoashutapea3@gmail.com>
2023-06-01 17:55:26 +0700

    tambah perubahan

--- Vaksin Tracking_05/VaksinTracking_05/VaksinTracking_05/Models/Vaksin_Entities.Designer.cs
+++ Vaksin Tracking_05/VaksinTracking_05/VaksinTracking_05/Models/Vaksin_Entities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Perkuliahan\TEKNET\Vaksin Tracking_05\VaksinTracking_05\VaksinTracking_05\Models\Vaksin_Entities.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Albert Manik\Perkuliahan\Semester 4\TEKNET\05_32TI01-32TI02-master\Vaksin Tracking_05\VaksinTracking_05\VaksinTracking_05\Models\Vaksin_Entities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
